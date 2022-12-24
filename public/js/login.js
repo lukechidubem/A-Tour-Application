351a525096ff070e37e866f44536f6e4d7d73e64
lukechidubem <lukechidubem@gmail.com>
2022-12-24 22:44:29 +0100

    Getting ready for deployment

--- public/js/login.js
+++ public/js/login.js
@@ -14,8 +14,8 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      // url: 'http://127.0.0.1:3000/api/v1/users/login',
-      url: `${baseUrl}${api}/users/login`,
+      url: '/api/v1/users/login',
+      // url: `${baseUrl}${api}/users/login`,
       data: {
         email,
         password
@@ -37,8 +37,8 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      // url: 'http://127.0.0.1:3000/api/v1/users/logout'
-      url: `${baseUrl}${api}/users/logout`
+      url: '/api/v1/users/logout'
+      // url: `${baseUrl}${api}/users/logout`
     });
     if ((res.data.status = 'success')) location.reload(true);
   } catch (err) {
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -10,10 +10,10 @@ export const updateSettings = async (data, type) => {
   try {
     const url =
       type === 'password'
-        ? // 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-          `${baseUrl}${api}/users/updateMyPassword`
-        : // 'http://127.0.0.1:3000/api/v1/users/updateMe'
-          `${baseUrl}${api}/users/updateMe`;
+        ? '/api/v1/users/updateMyPassword'
+        : // `${baseUrl}${api}/users/updateMyPassword`
+          '/api/v1/users/updateMe';
+    // `${baseUrl}${api}/users/updateMe`;
 
     const res = await axios({
       method: 'PATCH',
