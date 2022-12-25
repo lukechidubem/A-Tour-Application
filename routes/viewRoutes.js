b59ed555c86108593ef10b234666a23a0788ba10
lukechidubem <lukechidubem@gmail.com>
2022-12-25 02:29:52 +0100

    corrected price in webhook

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -17,12 +17,7 @@ router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/me', authController.protect, viewController.getAccount);
 
-router.get(
-  '/my-tours',
-  // bookingController.createBookingCheckout,
-  authController.protect,
-  viewController.getMyTours
-);
+router.get('/my-tours', authController.protect, viewController.getMyTours);
 
 router.post(
   '/submit-user-data',
